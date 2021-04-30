a6a5996425710c70a5f413339e75e3fd44818c3a
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2021-04-30 22:34:30 +0200

    pause 2

--- GameJam 2021/Assets/Scripts/Game/PausePanel.cs
+++ GameJam 2021/Assets/Scripts/Game/PausePanel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,41 @@ using UnityEngine;
 public class PausePanel : MonoBehaviour
 {
     public GameObject Panel;
+    public bool isInPause;
 
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isInPause == false)
+            {
+                Pause();
+            }
+
+            if (isInPause)
+            {
+                Resume();
+            }
+        }
+
+
+    }
 
     public void Pause()
     {
         playerController.Instance.movementIsEnabled = false;
+        cameraFollow.Instance.isActive = false;
         Panel.SetActive(true);
-        
+        isInPause = true;
+    }
+
+    public void Resume()
+    {
+        Panel.SetActive(false);
+        playerController.Instance.movementIsEnabled = true;
+        cameraFollow.Instance.isActive = true;
+        isInPause = false;
     }
     
 }
--- GameJam 2021/Assets/Scripts/Game/cameraFollow.cs
+++ GameJam 2021/Assets/Scripts/Game/cameraFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,33 @@ using UnityEngine;
 public class cameraFollow : MonoBehaviour
 {
 
+	public static cameraFollow Instance;
+	public bool isActive;
 	[SerializeField]
 	public Transform target;
 
 	public float smoothSpeed =12f;
 	public Vector3 Offset;
 
+	private void Awake()
+	{
+		Instance = this;
+	}
+
+	private void Start()
+	{
+		isActive = true;
+	}
+
 	private void LateUpdate()
 	{
-		Vector3 WantedPosition = target.position + Offset;
-		Vector3 smoothPosition = Vector3.Lerp(transform.position,WantedPosition,smoothSpeed * Time.deltaTime);
-		transform.position = smoothPosition;
+		if (isActive)
+		{
+			Vector3 WantedPosition = target.position + Offset;
+            		Vector3 smoothPosition = Vector3.Lerp(transform.position,WantedPosition,smoothSpeed * Time.deltaTime);
+            		transform.position = smoothPosition;
+		}
+		
 
 	}
 
