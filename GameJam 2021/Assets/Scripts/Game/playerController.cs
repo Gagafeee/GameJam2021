3f2a9ec07c127b1b80045ff71e5144c8ada2d4b7
Meliaris15 <thlmrsilvers@gmail.com>
2021-04-30 22:23:02 +0200

    movement player

--- GameJam 2021/Assets/Scripts/Game/playerController.cs
+++ GameJam 2021/Assets/Scripts/Game/playerController.cs
@@ -8,8 +8,17 @@ public class playerController : MonoBehaviour
     [SerializeField]
     float speed = 10f;
     [SerializeField]
+    public float jumpForce = 10f;
+
+
+    [SerializeField]
     Rigidbody2D controller;
+
     private Vector3 velocity = Vector3.zero;
+    public bool Jumping = false;
+    public bool isGrounded = false;
+    public bool movementIsEnabled = true;
+
     void Start()
 	{
         Application.targetFrameRate = 60;
@@ -17,13 +26,26 @@ public class playerController : MonoBehaviour
 
     void FixedUpdate()
 	{
-        float x = Input.GetAxis("Vertical");
-        float y = Input.GetAxis("Horizontal");
+        if (movementIsEnabled)
+        {
 
-        Vector3 moveVector = transform.up * x * speed +transform.right* y * speed;
 
-        controller.velocity= Vector3.SmoothDamp(controller.velocity,moveVector,ref velocity, .03f);
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jumping = true;
+                Debug.Log("wasabi");
+            }
+            float y = Input.GetAxis("Horizontal");
 
+            Vector3 moveVector = transform.right * y * speed;
+            controller.velocity = Vector3.SmoothDamp(controller.velocity, moveVector, ref velocity, .03f);
+
+            if (Jumping)
+            {
+                controller.AddForce(new Vector2(0f, jumpForce));
+                Jumping = false;
+            }
+        }
     }
 
 
