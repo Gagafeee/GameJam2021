28805e104022d370ba6f6173a233a2307b1bb95c
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2021-05-01 16:58:16 +0200

    PlateForme1

--- GameJam 2021/Assets/Scripts/Game/PlateformManager.cs
+++ GameJam 2021/Assets/Scripts/Game/PlateformManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Game;
 using UnityEngine;
 
 public class PlateformManager : MonoBehaviour
@@ -12,32 +13,67 @@ public class PlateformManager : MonoBehaviour
     public int size;
     public float speed;
     public Transform target;
+    public bool moveRedPlatform;
+    public int plateID;
+    public static PlateformManager instance;
+    public GameObject platObj;
+    public float platposy;
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Start()
     {
+        
+        plateID = 0;
+        moveRedPlatform = false;
         redPlateform = GameObject.FindGameObjectsWithTag("Plateform");
-        MovePlatform();
-
-        //target = (redPlateformPosX, 0,0);
+       MovePlatform();
 
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        
+        if (!moveRedPlatform) return;
+        Vector3 dir = target.position - redPlateform[plateID].transform.position;
+        redPlateform[plateID].transform.Translate(dir.normalized * (speed * Time.deltaTime), Space.World);
+            
+        if (Vector3.Distance(redPlateform[plateID].transform.position, target.position) < 0.2f)
         {
-            MovePlatform();
+            plateID++; 
+            
+            
         }
+        if (plateID == redPlateform.Length)
+        {
+            moveRedPlatform = false;
+            Destroy(GameObject.FindGameObjectWithTag("PlateformTargetPoint"));
+                        
+            plateID = 0;
+        }
+
     }
 
+    // ReSharper disable Unity.PerformanceAnalysis
     private void MovePlatform()
     {
 
         for (var i = 0; i < redPlateform.Length; i++)
         {
-            //GameObject.CreatePrimitive()
-            Vector3 dir = target.position - redPlateform[i].transform.position;
-            redPlateform[i].transform.Translate(dir.normalized * (speed * Time.deltaTime), Space.World);
+            
+            platposy = redPlateform[plateID].transform.position.y;
+            GameObject PlateformTarget = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            PlateformTarget.tag = "PlateformTargetPoint";
+            PlateformTarget.GetComponent<MeshRenderer>().enabled = false;
+            PlateformTarget.transform.position = redPlateform[plateID].transform.position;
+            PlateformTarget.name = "[AUTOGENERATE] plateform travel point";
+            target = PlateformTarget.transform;
+            moveRedPlatform = true;
         }
+         
+         
     }
+    
 }
