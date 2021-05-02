095918d8a38be4446a870f35140ddfbce344413c
Gagafeee <64372089+Gagafeee@users.noreply.github.com>
2021-05-02 15:49:40 +0200

    door2

--- GameJam 2021/Assets/Door.cs
+++ GameJam 2021/Assets/Door.cs
@@ -1,18 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        BorderMagager.instance.isActive = false;
+        SceneManager.LoadScene("Win");
+        Cursor.lockState = CursorLockMode.None;
     }
 }
